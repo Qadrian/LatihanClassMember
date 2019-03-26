511d6cacfa197a9969fbf6cc92f1c2950e24264b
Qadrian <ianqadrian@gmail.com>
2019-03-26 14:55:50 +0700

    Add files via upload

---
+++ LatihanMemberClass/LatihanMemberClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method 
+            taxi.TaxiInfo();
+            Console.WriteLine();
+
+            taxi.PickupPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LatihanMemberClass/LatihanMemberClass/Taxi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    public class Taxi
+    {
+        //properties
+
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", OnDuty ? "Yes" : "No")
+           /* if (OnDuty==true)
+            {
+                Console.WriteLine("On Duty: Yes");
+
+            }
+            else
+            {
+                Console.WriteLine("On Duty: No");
+            }
+            Console.WriteLine("Number of Passenger: {0}", NumPassenger);
+        }
+
+ `      */
+
+        public void PickupPassenger()
+         {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+          }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} sedang menurunkan penumpang", DriverName);
+        }
+    
+    }
+}
